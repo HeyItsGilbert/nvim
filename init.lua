a4e8dcc159d3227c91b581f4a3766dd5a41a542c
Gilbert Sanchez <me@gilbertsanchez.com>
2024-11-03 18:10:36 -0800

    Add settings to powershell LSP

--- init.lua
+++ init.lua
@@ -614,7 +614,13 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        powershell_es = {},
+        powershell_es = {
+          settings = {
+            powershell = {
+              codeFormatting = { Preset = 'OTBS' },
+            },
+          },
+        },
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
